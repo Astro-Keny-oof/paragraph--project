0e57d40cb51192f959c7197d0ddeb440edfd22d6
Kanwar <76886563+Astro-Keny-oof@users.noreply.github.com>
2021-02-25 20:28:46 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function getparagraph1(){
+    var inputs=[]
+    for(var i=1; 1=6;1++)
+    inputs.push(document.getElementById("para1_input_box"+i).value);
+    inputs.join(". ")
+    document.getElementById("showParagraph").innerHTML=inputs.join(". ");
+}
+function getparagraph2(){
+    var inputs=[]
+    for(var i=1; 1=6;1++)
+    inputs.push(document.getElementById("para1_input_box"+i).value);
+    inputs.join(". ")
+    document.getElementById("showParagraph").innerHTML=inputs.join(". ");
+}
+function getparagraph3(){
+    var inputs=[]
+    for(var i=1; 1=6;1++)
+    inputs.push(document.getElementById("para1_input_box"+i).value);
+    inputs.join(". ")
+    document.getElementById("showParagraph").innerHTML=inputs.join(". ");
+}
+function getparagraph5(){
+    var inputs=[]
+    for(var i=1; 1=6;1++)
+    inputs.push(document.getElementById("para1_input_box"+i).value);
+    inputs.join(". ")
+    document.getElementById("showParagraph").innerHTML=inputs.join(". ");
+}
+function getparagraph6(){
+    var inputs=[]
+    for(var i=1; 1=6;1++)
+    inputs.push(document.getElementById("para1_input_box"+i).value);
+    inputs.join(". ")
+    document.getElementById("showParagraph").innerHTML=inputs.join(". ");
+}
